4ef21fb998b196fe969ac2f175e76d670db8b7ec
Gaurav Kumar Rai <34624495+gaurav09kumar@users.noreply.github.com>
2019-06-26 18:13:28 +0530

    Update Table3.java

--- Table3.java
+++ Table3.java
@@ -2,14 +2,14 @@ package jack;
 
 public class Table3 {
 	public static void main(String args[]) {
-		for(int i=1;i<=4;i++){
-			for(int j=0;j<=3-i;j++) {
+		for(int i=1;i<5;i++){
+			for(int j=0;j<5-i;j++) {
 				System.out.print(" ");
 			}
 			for(int k=0;k<i;k++) {
 				System.out.print(i+" ");
 			}
-			System.out.print("\n");
+			System.out.println("");
 		}
 	}
 }
