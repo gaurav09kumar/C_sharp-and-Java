bebbbbe9b3cfe8fea3f0062a9a3022984514ed89
Gaurav Kumar Rai <34624495+gaurav09kumar@users.noreply.github.com>
2019-06-28 09:56:05 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public partial class Class1
+    {
+        private string Author_name;
+        private int Total_articles;
+
+        public Class1(string a, int t)
+        {
+            this.Author_name = a;
+            this.Total_articles = t;
+        }
+    }
+
+}
---
+++ Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public partial class Class1
+    {
+        public void Display()
+        {
+            Console.WriteLine("Author's name is : " + Author_name);
+            Console.WriteLine("Total number articles is : " + Total_articles);
+        }
+    }
+
+
+}
